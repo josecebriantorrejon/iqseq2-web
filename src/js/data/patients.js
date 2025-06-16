d8e04c61f17bb657c31e3be60697f056bdb633db
Jose Cebrian <jcebrian@jcebria-ltmkcnn.internal.salesforce.com>
2025-06-16 18:59:05 +0200

    refactoring

--- src/js/data/patients.js
+++ src/js/data/patients.js
@@ -6,7 +6,7 @@ export const pacientesData = [
         edad: "2019",
         genero: "Femenino",
         historia: "Maia es una niña muy risueña y juguetona. Le encanta pasar tiempo al aire libre y disfrutar de la naturaleza. A pesar de los desafíos, siempre tiene una sonrisa y nos enseña a todos el valor de la resiliencia.",
-        imagen: "images/superheroes/MAIA.jpg"
+        imagen: "assets/images/superheroes/MAIA.jpg"
     },
     {
         nombre: "JORDI",
@@ -15,7 +15,7 @@ export const pacientesData = [
         edad: "2012",
         genero: "Masculino",
         historia: "Jordi es un niño enérgico con una pasión por la música. Disfruta de la compañía de sus amigos y siempre está dispuesto a aprender cosas nuevas. Su optimismo es contagioso y es una inspiración para todos.",
-        imagen: "images/superheroes/JORDI.jpg"
+        imagen: "assets/images/superheroes/JORDI.jpg"
     },
     {
         nombre: "JUDITH",
@@ -24,7 +24,7 @@ export const pacientesData = [
         edad: "2015",
         genero: "Femenino",
         historia: "Judith es una niña creativa y curiosa, le encanta dibujar y contar historias. Su imaginación no tiene límites y su alegría ilumina cada día. Es un ejemplo de perseverancia y amor.",
-        imagen: "images/superheroes/JUDITH.jpeg"
+        imagen: "assets/images/superheroes/JUDITH.jpeg"
     },
     {
         nombre: "NOAH",
@@ -33,7 +33,7 @@ export const pacientesData = [
         edad: "2022",
         genero: "Masculino",
         historia: "Noah es un bebé lleno de energía y vitalidad, siempre explorando el mundo con una sonrisa. Su espíritu aventurero nos inspira a disfrutar cada pequeño momento.",
-        imagen: "images/superheroes/NOAH.jpg"
+        imagen: "assets/images/superheroes/NOAH.jpg"
     },
     {
         nombre: "ADRIANA",
@@ -42,7 +42,7 @@ export const pacientesData = [
         edad: "2018",
         genero: "Femenino",
         historia: "Adriana es una niña valiente y decidida, con una personalidad vibrante. Le encanta aprender y jugar, demostrando cada día su fuerza y ganas de vivir. Es un ejemplo de superación constante.",
-        imagen: "images/superheroes/ADRIANA.jpeg"
+        imagen: "assets/images/superheroes/ADRIANA.jpeg"
     },
     {
         nombre: "MIQUEL",
@@ -51,7 +51,7 @@ export const pacientesData = [
         edad: "2023",
         genero: "Masculino",
         historia: "Miquel es un pequeño luchador, con una mirada llena de esperanza y un espíritu inquebrantable. Su sonrisa es un faro de alegría para su familia, que le acompaña en cada paso de su camino.",
-        imagen: "images/superheroes/MIQUEL.jpeg"
+        imagen: "assets/images/superheroes/MIQUEL.jpeg"
     },
     {
         nombre: "JOEL",
@@ -60,7 +60,7 @@ export const pacientesData = [
         edad: "2018",
         genero: "Masculino",
         historia: "Joel es un niño activo y curioso, siempre buscando nuevas aventuras. Su energía y determinación son una fuente de inspiración para todos los que le rodean. Su espíritu alegre es contagioso.",
-        imagen: "images/superheroes/JOEL.jpeg"
+        imagen: "assets/images/superheroes/JOEL.jpeg"
     },
     {
         nombre: "BRAIS",
@@ -69,7 +69,7 @@ export const pacientesData = [
         edad: "2016",
         genero: "Masculino",
         historia: "Brais es un niño lleno de vitalidad y una sonrisa que ilumina cualquier lugar. A pesar de los desafíos, siempre encuentra la manera de disfrutar y compartir su alegría con los demás.",
-        imagen: "images/logos/LOGOTIPO-AEIQSEC2-VERTICAL.svg"
+        imagen: "assets/images/logos/LOGOTIPO-AEIQSEC2-VERTICAL.svg"
     },
     {
         nombre: "DARIO",
@@ -78,6 +78,6 @@ export const pacientesData = [
         edad: "2015",
         genero: "Masculino",
         historia: "Dario es un niño que nos sorprende cada día con su capacidad de superación y su espíritu incansable. Su dulzura y su cariño son un regalo para quienes le rodean.",
-        imagen: "images/logos/LOGOTIPO-AEIQSEC2-VERTICAL.svg"
+        imagen: "assets/images/logos/LOGOTIPO-AEIQSEC2-VERTICAL.svg"
     }
 ]; 
